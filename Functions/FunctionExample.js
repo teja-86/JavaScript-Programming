aa4390a640e45ef763f8dabdc846afb61607a533
teja-86 <134760904+teja-86@users.noreply.github.com>
2023-08-10 14:29:51 +0530

    Delete Functions directory

--- Functions/FunctionExample.js
+++
@@ -1,5 +0,0 @@
-function sum(a,b){
-    console.log(a + b);
-}
-
-var result = sum(9,10);
--- Functions/FunctionWithReturnType.js
+++
@@ -1,9 +0,0 @@
-function add(){
-    let a = 10;
-    let b = 20;
-    return a + b;
-}
-
-var a = add();
-console.log(a);
-
