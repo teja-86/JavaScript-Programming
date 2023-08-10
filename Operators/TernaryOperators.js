bd30c4718a9f665442675e2c52dbbe4da44764e1
teja-86 <134760904+teja-86@users.noreply.github.com>
2023-08-10 14:36:44 +0530

    Delete Operators directory

--- Operators/TernaryOperators.js
+++
@@ -1,4 +0,0 @@
-let b = 10;
-let c = 20;
-
-(b<c) ? console.log("True") : console.log("False");;
--- Operators/TypeOfOperators.js
+++
@@ -1,8 +0,0 @@
-var a = typeof "Hello";
-console.log(a);
-
-var b = typeof 12;
-console.log(b);
-
-var c = typeof true;
-console.log(c);
